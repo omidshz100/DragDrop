fc67c31bff935ac373f945e603ed8eb7ccc0e945
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-08-25 14:22:30 +0200

    Initial Commit

---
+++ DragDrop/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DragDrop
+//
+//  Created by Omid Shojaeian Zanjani on 25/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DragDrop/DragDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropApp.swift
+//  DragDrop
+//
+//  Created by Omid Shojaeian Zanjani on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DragDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
